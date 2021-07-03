6d84ba10da854c72f37d9573577a7490f87feac1
Harrison Wheeler <wheelerharrison93@gmail.com>
2021-07-03 13:09:39 -0600

    initial build

---
+++ lot33.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'bcw-harrisonw.us.auth0.com'
+export const audience = 'https://lot33.com'
+export const clientId = 'NBBVkVQmU7cS4NuTcDPbqkMfo8hJZCFx'
